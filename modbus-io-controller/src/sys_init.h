6df5a45f1fd7f7bbbb724ce52e77f8850912d06f
Jeremy Peake <peakeelectronicinnovation@gmail.com>
2025-09-20 14:33:13 +1200

    Version update to v1.1.2

--- modbus-io-controller/src/sys_init.h
+++ modbus-io-controller/src/sys_init.h
@@ -6,11 +6,11 @@
 #endif
 
 #ifndef VERSION_MINOR
-    #define VERSION_MINOR 0
+    #define VERSION_MINOR 1
 #endif
 
 #ifndef VERSION_PATCH
-    #define VERSION_PATCH 0
+    #define VERSION_PATCH 2
 #endif
 
 // Helper macros for proper string handling
